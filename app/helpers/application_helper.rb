8072a784a1b7b1492b2256547dc32152c6899b67
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2025-09-25 19:03:32 -0300

    Add full title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     component_class_name = component_string.split("\\").map(&:camelcase).join("::")
     render "#{component_class_name}Component".constantize.new(**args)
   end
+
+  def full_title(page_title = '', base_title = t('app.name'))
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
