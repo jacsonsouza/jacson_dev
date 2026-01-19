d3d5399c6a7ac2be3e906a2415815163e7991e48
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-01-19 15:42:04 -0300

    Fix a broken system test on login form.

--- test/system/authentication/user_sessions_test.rb
+++ test/system/authentication/user_sessions_test.rb
@@ -8,7 +8,7 @@ class UserSessionsTest < ApplicationSystemTestCase
   test 'should display login form' do
     visit new_user_session_path
 
-    assert_selector 'img[alt="Jacson Dev Logo"]'
+    assert_selector 'img[alt="Jacson Dev"]'
 
     within('form') do
       assert_selector 'input[name="user[email]"]'
