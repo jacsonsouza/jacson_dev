0a3980e92b87cbd924fb0a97587042d90d5776a4
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-01-27 11:31:52 -0300

    Add form for projects creation.

---
+++ app/javascript/controllers/tom_select_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from '@hotwired/stimulus';
+
+// Connects to data-controller="tom-select"
+export default class extends Controller {
+   connect() {
+      new TomSelect(this.element, {
+         plugins: ['remove_button'],
+         placeholder: 'Selecione',
+      });
+   }
+}
