342e6bdc310df29ddee352126d8b94eaea4595e3
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-02-11 14:51:34 -0300

    Fix breadcrumbs missing

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -15,11 +15,18 @@ class Users::BaseController < ApplicationController
     set_breadcrumbs if respond_to?(:set_breadcrumbs, true)
   end
 
-  def set_breadcrumbs
-    set_resource_breadcrumbs(
-      resource: :skill,
-      collection_path: users_skills_path,
-      instance: @skill
-    )
+  def set_resource_breadcrumbs(resource:, collection_path:, instance: nil)
+    key = resource.to_s.pluralize
+
+    add_breadcrumb t("breadcrumbs.#{key}.index"), collection_path
+
+    case action_name.to_sym
+    when :new
+      add_breadcrumb t("breadcrumbs.#{key}.new")
+    when :edit
+      add_breadcrumb t("breadcrumbs.#{key}.edit", name: instance&.name)
+    when :show
+      add_breadcrumb instance&.name
+    end
   end
 end
