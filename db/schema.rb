b5696867e78b53e57f33044d304a71053bd9fccb
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-04-13 16:32:33 -0300

    upgrade ruby to 4.0.2 version

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_19_213955) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_19_213955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -60,17 +60,17 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_19_213955) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "short_description"
     t.integer "category", default: 0
-    t.date "start_date", null: false
-    t.date "end_date"
-    t.string "url"
-    t.string "repository"
-    t.boolean "favorite", default: false, null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
+    t.date "end_date"
+    t.boolean "favorite", default: false, null: false
+    t.string "name", null: false
+    t.string "repository"
+    t.text "short_description"
+    t.date "start_date", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
+    t.bigint "user_id", null: false
     t.index ["favorite"], name: "index_projects_on_favorite"
     t.index ["name"], name: "index_projects_on_name"
     t.index ["start_date", "end_date"], name: "index_projects_on_start_date_and_end_date"
@@ -78,55 +78,55 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_19_213955) do
   end
 
   create_table "skill_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.bigint "skill_id", null: false
     t.bigint "tag_id", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["skill_id"], name: "index_skill_tags_on_skill_id"
     t.index ["tag_id"], name: "index_skill_tags_on_tag_id"
   end
 
   create_table "skills", force: :cascade do |t|
-    t.string "name"
     t.integer "category", default: 0
-    t.text "short_description"
     t.string "color"
-    t.integer "experience_time"
-    t.integer "proficiency"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
+    t.integer "experience_time"
+    t.string "name"
+    t.integer "proficiency"
+    t.text "short_description"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["name"], name: "index_skills_on_name", unique: true
     t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.datetime "confirmation_sent_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "current_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
+    t.datetime "last_sign_in_at"
+    t.string "last_sign_in_ip"
     t.datetime "locked_at"
     t.string "name"
-    t.datetime "created_at", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0, null: false
+    t.string "unconfirmed_email"
+    t.string "unlock_token"
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
