fd75cf4f65fa823f56a3a639d2c98b23babd5ffc
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-07-22 15:26:20 -0300

    feat: add read and unread filters to message index page.

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -1,12 +1,12 @@
 class Users::MessagesController < Users::BaseController
+  before_action :set_message, only: [:destroy, :toggle_read]
+
   def index
-    @messages = Message.order(created_at: :desc)
+    @messages = Message.filter_by_read(params[:read]).order(created_at: :desc)
   end
 
   def destroy
-    message = Message.find(params[:id])
-
-    if message.destroy
+    if @message.destroy
       redirect_to users_messages_path,
                   notice: t('notices.destroy', resource: message.model_name.human)
     else
@@ -15,13 +15,17 @@ class Users::MessagesController < Users::BaseController
   end
 
   def toggle_read
-    message = Message.find(params[:id])
-
-    if message.update(read: !message.read)
+    if @message.update(read: !message.read)
       redirect_to users_messages_path,
                   notice: t('notices.update', resource: message.model_name.human)
     else
       redirect_to users_messages_path, alert: t('alert.delete_message_error')
     end
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :subject, presence: true, length: { minimum: 5, maximum: 255 }
   validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
+
+  scope :filter_by_read, ->(read) { where(read: read) if read.present? }
 end
