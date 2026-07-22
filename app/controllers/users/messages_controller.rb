0c5780c42c2b85262238b09c76b6366c978fa673
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-07-22 15:50:06 -0300

    feat: add tests for messages controller

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -8,16 +8,16 @@ class Users::MessagesController < Users::BaseController
   def destroy
     if @message.destroy
       redirect_to users_messages_path,
-                  notice: t('notices.destroy', resource: message.model_name.human)
+                  notice: t('notices.destroy', resource: @message.model_name.human)
     else
       redirect_to users_messages_path, alert: t('alert.delete_message_error')
     end
   end
 
   def toggle_read
-    if @message.update(read: !message.read)
+    if @message.update(read: !@message.read)
       redirect_to users_messages_path,
-                  notice: t('notices.update', resource: message.model_name.human)
+                  notice: t('notices.update', resource: @message.model_name.human)
     else
       redirect_to users_messages_path, alert: t('alert.delete_message_error')
     end
