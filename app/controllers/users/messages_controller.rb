4f19aea989a14edb5ec259c214f1137bd796b1fe
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-07-22 14:57:01 -0300

    feat: add two new actions for message management, delete and toggle read.

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -2,4 +2,26 @@ class Users::MessagesController < Users::BaseController
   def index
     @messages = Message.order(created_at: :desc)
   end
+
+  def destroy
+    message = Message.find(params[:id])
+
+    if message.destroy
+      redirect_to users_messages_path,
+                  notice: t('notices.destroy', resource: message.model_name.human)
+    else
+      redirect_to users_messages_path, alert: t('alert.delete_message_error')
+    end
+  end
+
+  def toggle_read
+    message = Message.find(params[:id])
+
+    if message.update(read: !message.read)
+      redirect_to users_messages_path,
+                  notice: t('notices.update', resource: message.model_name.human)
+    else
+      redirect_to users_messages_path, alert: t('alert.delete_message_error')
+    end
+  end
 end
